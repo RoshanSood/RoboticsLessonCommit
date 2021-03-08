13b6056adfaba981114ba5b6816f2586b88f4e9b
Roshan Sood <legodude2774@gmail.com>
2021-03-07 16:17:36 -0800

    Updated Code

---
+++ SoftwareRules.java
@@ -0,0 +1,5 @@
+public class SoftwareRules{
+  public static void main(String[] args) {
+    System.out.println("engineering is bad and software rules since we program the entire robot and make it work");
+  }
+}
