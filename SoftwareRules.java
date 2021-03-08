00f22ad25e002fdea74469154356113926527507
Roshan Sood <legodude2774@gmail.com>
2021-03-07 16:21:16 -0800

    Added Random

--- SoftwareRules.java
+++ SoftwareRules.java
@@ -1,5 +1,8 @@
+import java.util.Random;
+
 public class SoftwareRules{
   public static void main(String[] args) {
-    System.out.println("engineering is bad and software rules since we program the entire robot and make it work");
+    Random ran = new Random();
+    System.out.println(ran.nextInt(3));
   }
 }
